276a3f69d726d127470a76758ccbc6d6a4fe9ae9
warja69 <thomas.yousef@elev.ga.ntig.se>
2018-09-19 14:50:40 +0200

    Primitiva

---
+++ src/printingnumbers/primitiva3.java
@@ -0,0 +1,14 @@
+package printingnumbers;
+
+public class primitiva3 {
+
+	public static void main(String[] args) {
+	double dnum = 8.99999;
+    if((dnum % 1) == 0) {
+     System.out.println("It's not a double number");
+    } else {
+       System.out.println("It's a double number");
+    }
+	}
+
+}
