2c6bd49c7eeb18f13237c5391ecf8584f8648764
warja69 <thomas.yousef@elev.ga.ntig.se>
2018-10-04 09:41:53 +0200

    Input

--- src/Input/Input10.java
+++ src/Input/Input10.java
@@ -1,10 +1,30 @@
 package Input;
 
+import java.util.Scanner;
+
 public class Input10 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+        int dec_num, rem, quot, i=1, j;
+        int bin_num[] = new int[100];
+        Scanner scan = new Scanner(System.in);
+		
+        System.out.print("Input a Decimal Number : ");
+        dec_num = scan.nextInt();
+		
+        quot = dec_num;
+		
+        while(quot != 0)
+        {
+            bin_num[i++] = quot%2;
+            quot = quot/2;
+        }
+		
+        System.out.print("Binary number is: ");
+        for(j=i-1; j>0; j--)
+        {
+            System.out.print(bin_num[j]);
+        }
+        System.out.print("\n");
+    }
 }
--- src/Input/Input14.java
+++ src/Input/Input14.java
@@ -1,6 +1,6 @@
 package Input;
 
-public class Input9 {
+public class Input14 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
