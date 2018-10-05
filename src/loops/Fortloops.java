b56a9e88bc5c943635d5ae9ec5f4a89c747eeaa7
warja69 <thomas.yousef@elev.ga.ntig.se>
2018-10-05 10:29:26 +0200

    fortloops

---
+++ src/loops/Fortloops.java
@@ -0,0 +1,60 @@
+package loops;
+
+public class Fortloops {
+
+	public static void main(String[] args) {
+	   /*
+		int number = 0;
+		
+		System.out.println(number);
+		number = number + 1
+		System.out.println(number);
+		number += number;
+		System.out.println(number);
+		number++;
+		System.out.println(number);
+		*/
+		
+		
+		
+		/*
+		 * for ( startvärde; slutvärde; förändring) {
+		 * this is where everything happens many times!
+		 * }
+		 */
+		for(int i = 1; i <=100; i++) {
+			System.out.println(i);
+			
+		}
+		boolean alive = true;
+		/*
+		for(;;) {
+			
+			if( alive == false) {
+				break;
+				}
+			}*/
+			
+		boolean gameOn = true;
+		
+		while (true) {
+			/*
+			 alive = false;
+			 if (alive == false) {
+				 break;
+				 System.out.println("Game over")!
+				 break;
+				 }
+			*/
+			/*
+			 alive = false;
+			 if (alive == false) {
+				 break;
+				 System.out.println("Well done")!
+				 }
+			*/
+		}
+           
+	}
+
+}
