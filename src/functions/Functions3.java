ae57bf4c40fdbf9e2b3539b790b09a0a5ad29394
warja69 <thomas.yousef@elev.ga.ntig.se>
2018-11-19 11:18:15 +0100

    arrays

--- src/functions/Functions3.java
+++ src/functions/Functions3.java
@@ -4,7 +4,7 @@ public class Functions3 {
 
 	public static void main(String[] args) {
 		String phrase = new String ("350.");
-		String middle3 = new String ("one"); //I want to print the middle 3 characters in "phrase" which I think is "tri".
+		String middle3 = new String ("tri"); //I want to print the middle 3 characters in "phrase" which I think is "tri".
 
 		middle3 = phrase.substring (1, 2); // this only prints out 1 letter instead of 3
 
